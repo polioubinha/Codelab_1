df78e17eff61bcd4ac915727b6303f2e6b3163f8
Poli <polioubinha@gmail.com>
2023-11-28 15:32:56 +0100

    Métodos para la implementación del toolbar en la vista

--- app/src/main/java/com/pmdm/codelab1/ProductGridFragment.java
+++ app/src/main/java/com/pmdm/codelab1/ProductGridFragment.java
@@ -1,10 +1,10 @@
 package com.pmdm.codelab1;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.view.*;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 public class ProductGridFragment extends Fragment {
@@ -13,6 +13,34 @@ public class ProductGridFragment extends Fragment {
     public View onCreateView(
             @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.shr_product_grid_fragment, container, false);
+        View view = inflater.inflate(R.layout.shr_product_grid_fragment, container, false);
+
+        //Iniciamos el toolbar
+        setUpToolbar(view);
+
+        return view;
     }
+
+    //Este método se encarga de configurar la barra de herramientas en la vista proporcionada.
+    private void setUpToolbar(View view){
+        Toolbar toolbar = view.findViewById(R.id.app_bar);
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        if(activity != null){
+            activity.setSupportActionBar(toolbar);
+        }
+    }
+
+    //Este método se utiliza para inflar el menú de opciones en la barra de herramientas.
+    public void onCreateOptionsMenu(Menu menu, MenuInflater menuInflater){
+        menuInflater.inflate(R.menu.shr_toolbar_menu, menu);
+        super.onCreateOptionsMenu(menu, menuInflater);
+    }
+
+    //Configuramos el toolbar de nuestro diseño XML para esta actividad
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
 }
